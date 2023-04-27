d93caa05ad819e4dcca32d82428b1d7c2ad80d8b
Etarskova <etarskova@gmail.com>
2023-04-27 10:09:31 +0300

    added files

---
+++ true.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="style.css">
+  <Body> 
+  <h1>Таблица истинности PHP</h1>
+    <table class="table">
+      <tbody>
+        <tr>
+          <td>Ячейка 1</td>
+          <td>Ячейка 2</td>
+          <td>Ячейка 3</td>
+        </tr>
+        <tr>
+          <td>Ячейка 4</td>
+          <td>Ячейка 5</td>
+          <td>Ячейка 6</td>
+        </tr>
+        <tr>
+          <td>Ячейка 7</td>
+          <td>Ячейка 8</td>
+          <td>Ячейка 9</td>
+        </tr>
+      </tbody>
+    </table>
+  </Body>
+
+  <footer>
+        <div class="links">
+            <a href="index.html">На главную</a>
+        </div>
+    </footer>
+  
+</html>
+
+
